7f008b24689574bde060bac01ec2c607720129a5
radifanmp <radifanmp@gmail.com>
2020-04-07 11:19:01 +0700

    Frist Commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Increment from './increment';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Increment />
   </React.StrictMode>,
   document.getElementById('root')
 );
